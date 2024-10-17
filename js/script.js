0f45b203aa7cdd06c3223be10259e710bc40ca9c
sneka <sneka1606@gmail.com>
2024-10-18 01:37:23 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
+const appSettings ={
+    databaseURL: "https://js-crud-de722-default-rtdb.firebaseio.com/",
+}
